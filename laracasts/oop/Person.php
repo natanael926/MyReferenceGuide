625581a0a7cb426b604e08415637dc9da16de139
Rdusy Natanael Acosta Crousset <natanael926@gmail.com>
2016-06-01 07:27:16 -0400

    Mether getter and seeter

---
+++ laracasts/oop/Person.php
@@ -0,0 +1,33 @@
+<?php 
+
+class Person {
+
+    public $name;
+
+    public $age;
+
+    public function __construct($name) 
+    {
+        $this->name = $name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age) 
+    {
+        if($age < 18)
+        {
+            throw new Exception("Person is not old enough.");
+        }
+
+        $this->age = $age;
+    }
+}
+
+$john = new Person('John Doe');
+$john->setAge(30); 
+
+var_dump($john);
