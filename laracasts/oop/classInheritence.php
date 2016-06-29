2a1ee828287179b56c5d3b26d2878dc26545af20
Rdusy Natanael Acosta Crousset <natanael926@gmail.com>
2016-06-29 12:21:02 -0400

    Class Inheritance

--- laracasts/oop/classInheritence.php
+++ laracasts/oop/classInheritence.php
@@ -1,2 +1,21 @@
 <?php
 
+class Share
+{
+
+}
+
+class Square extends Share
+{
+    protected $length = 4;
+
+    public function getArea()
+    {
+        return pow($this->length, 2);
+    }
+}
+
+
+
+echo (new Share)->getArea();
+
